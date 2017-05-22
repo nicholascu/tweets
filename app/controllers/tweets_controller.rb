5fe287e82bba22f3e00059a83c3bcb29556ec4aa
Nicholas Cu <nicholascu@gmail.com>
2017-05-21 18:39:17 -0700

    add basic log to retrieve user and tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,20 @@
 class TweetsController < ApplicationController
 	def index
+		@username = params[:search]
+		@twitter_user = $twitter.user(@username)
+		@twitter_timeline = $twitter.user_timeline(@twitter_user, {count:25})
+		expires_in 5.minutes, :public => true
 	end
 
 	def show
+		@twitter_timeline = $twitter.user_timeline(@twitter_user, {count:25})
+		expires_in 5.minutes, :public => true
 	end
+
+	# def create
+	# 	 render plain: params[:tweets].inspect
+	# end
 end
+
+
+# Parameters: {"utf8"=>"✓", "search"=>"elonmusk", "commit"=>"Search"}
