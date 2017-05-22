768d0f801018c6e24a4d14248320dfbc0197e66c
Nicholas Cu <nicholascu@gmail.com>
2017-05-21 18:39:37 -0700

    add tweets resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   resources :tweets
 
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'tweets#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
