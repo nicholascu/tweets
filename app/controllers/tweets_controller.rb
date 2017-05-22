d893f2f9a74bb9359dc4cb579fc72644154fadf3
Nicholas Cu <nicholascu@gmail.com>
2017-05-22 09:39:36 -0700

    add rescue if user not found

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,9 @@ class TweetsController < ApplicationController
 		@twitter_user = $twitter.user(@username ||= "StackCommerce")
 		@twitter_timeline = $twitter.user_timeline(@twitter_user, {count:25})
 		expires_in 5.minutes, :public => true
+	rescue Twitter::Error::NotFound => e
+		flash[:notice] = "USER NOT FOUND"
+		redirect_to(:action => 'index')
 	end
 
 	def show
